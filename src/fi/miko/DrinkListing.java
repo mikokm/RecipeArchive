158277fff645f78233e1bd3e9d01bd83f2254b4d
Miko Mynttinen <miko.mynttinen@helsinki.fi>
2014-08-17 11:48:46 +0300

    Im lazy and added eclipse project files

---
+++ src/fi/miko/DrinkListing.java
@@ -0,0 +1,55 @@
+package fi.miko;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DrinkListing
+ */
+@WebServlet("/DrinkListing")
+public class DrinkListing extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DrinkListing() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter(); 
+		try {
+			/* TODO output your page here. You may use following sample code. */
+			out.println("<html>"); 
+			out.println("<head><title>Servlet TestiServlet</title></head>");
+			out.println("<body>");
+			out.println("<h1>Servlet TestiServlet at " + request.getContextPath() + "</h1>");
+			out.println("</body>");
+			out.println("</html>");
+		} finally {            
+			out.close();
+		}
+	}
+}
